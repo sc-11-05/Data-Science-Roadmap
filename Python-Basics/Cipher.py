9e30067d0ca62a0809fee7bf7b8267e51ac70983
Sujal Chowdhary <sujalc1105@gmail.com>
2025-09-19 12:27:03 +0530

    Day1

---
+++ Python-Basics/Cipher.py
@@ -0,0 +1,41 @@
+# This code helps to understand about string manipulation and basic fundamentals of python
+
+# What this program do?
+# This program helps to encrypt and decrypt the text which can be used in cybersecurity for text encryption while sending messages or data.
+
+text = 'mrttaqrhknsw ih puggrur'
+custom_key = 'python'
+
+def vigenere(message, key, direction=1):
+    key_index = 0
+    alphabet = 'abcdefghijklmnopqrstuvwxyz'
+    final_message = ''
+
+    for char in message.lower():
+
+        # Append any non-letter character to the message
+        if not char.isalpha():
+            final_message += char
+        else:        
+            # Find the right key character to encode/decode
+            key_char = key[key_index % len(key)]
+            key_index += 1
+
+            # Define the offset and the encrypted/decrypted letter
+            offset = alphabet.index(key_char)
+            index = alphabet.find(char)
+            new_index = (index + offset*direction) % len(alphabet)
+            final_message += alphabet[new_index]
+    
+    return final_message
+
+def encrypt(message, key):
+    return vigenere(message, key)
+    
+def decrypt(message, key):
+    return vigenere(message, key, -1)
+
+print(f'\nEncrypted text: {text}')
+print(f'Key: {custom_key}')
+decryption = decrypt(text, custom_key)
+print(f'\nDecrypted text: {decryption}\n')
--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # Data-Science-Roadmap
 My step-by-step journey to becoming a Data Scientist &amp; ML Engineer — learning, projects, and notes.
+
+# Repository structures
+- Python-Basics -> Python Exercises & scripts
+- Stats-Basics -> Probability and Statistics practice
+- Git-Practice -> Git/Github learning notes
+- Phase1-projects -> Mini projects
+(More notes will be added for future phase)
